2ae8fdbeb94bed891e71c5474258b11a88ac0155
taijulsir <princetaijul123@gmail.com>
2025-12-10 02:20:55 +0000

    Adding comprehensive error handling

---
+++ src/Utilities/APIs/ItemsAPIs/ItemGroupsAPIs/CollectionsAPIs/CollectionsAPIs.ts
@@ -0,0 +1,4 @@
+import { ITEM_GROUP_API } from "../ItemGroupsAPIs";
+
+export const COLLECTIONS_API =ITEM_GROUP_API + 'collections/'
+export const ITEM_COLLECTIONS_API =COLLECTIONS_API + 'itemCollections/'
