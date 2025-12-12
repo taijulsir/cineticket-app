7b8b8220c88532ee076eed580db9d5d2e7142e75
taijulsir <princetaijul123@gmail.com>
2025-12-12 05:22:39 +0000

    Improving component reusability

---
+++ src/app/event/[slug]/components/MoviesPage/components/TheatricalSection/components/ToofanAtHoytsInAustralia/ToofanAtHoytsInAustralia.tsx
@@ -0,0 +1,41 @@
+
+import "./ToofanHoytsInAustralia.css"
+function ToofanAtHoytsInAustralia({
+    name, setName,
+    email, setEmail,
+    mobileNumber, setMobileNumber
+}) {
+    return (
+        // <div className="hoyts_contact_form w-full">
+        <div 
+        className="grid grid-cols-1  gap-4 mb-4
+        sm:grid-cols-[repeat(auto-fill,minmax(150px,1fr))] 
+        md:grid-cols-[repeat(auto-fill,minmax(200px,1fr))] 
+       ">
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Enter Name"
+                className="input_field"
+            />
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter Email"
+                className="input_field"
+            />
+            <input
+                type="text"
+                value={mobileNumber}
+                onChange={(e) => setMobileNumber(e.target.value)}
+                placeholder="Enter Phone"
+                className="input_field"
+            />
+
+        </div>
+    )
+}
+
+export default ToofanAtHoytsInAustralia
