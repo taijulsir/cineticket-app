eddd0db40037d82d33c438aab717c8ef78be3e5e
taijulsir <princetaijul123@gmail.com>
2026-01-07 14:37:04 +0000

    Enhancing mobile responsiveness

---
+++ src/app/event/[slug]/components/MoviesPage/components/SelectShows/components/Pricing/utils/hooks/useApplyPromocode.ts
@@ -0,0 +1,70 @@
+import { useContext } from "react";
+
+import AppContext from '@/context/AppContext';
+import useAxiosPublicInstance from "@/Utilities/Hooks/AxiosInstanceHooks/useAxiosPublicInstance";
+
+function useApplyPromocode(promoCode, setPromoCode, error, setError, triggerFetch, setName, setEmail, setMobileNumber) {
+
+    const axiosPublicInstance = useAxiosPublicInstance();
+
+    const { setDiscount, afterDiscountTotal, discount, promoCodeId, setPromoCodeId, setAfterDiscountTotal, isPromo, setIsPromo, selectedSeats, setSelectedSeats } = useContext(AppContext)
+
+    const handleApplyPromoCode = async (total) => {
+        try {
+            const { data } = await axiosPublicInstance.post("/applyPromocode", {
+                promoCode,
+                totalPrice: total,
+                selectedSeats
+            });
+
+            setDiscount(data?.discount || 0);
+            setAfterDiscountTotal(data?.totalPrice || 0);
+            setIsPromo(true);
+            setPromoCodeId(data?.promoCodeId || '');
+
+            if (data.error) {
+                setError(data.message);
+                setSelectedSeats([])
+                setName("")
+                setEmail("")
+                setMobileNumber("")
+                triggerFetch()
+            } else {
+                setError("")
+            }
+        } catch (error) {
+            if (
+                error.response &&
+                error.response.data &&
+                error.response.data.message
+            ) {
+                setError(error.response.data.message);
+            } else {
+                setError("An error occurred while applying the promo code. Please try again.");
+            }
+        }
+    };
+
+    const resetPromoCode = () => {
+        setPromoCode('');
+        setPromoCodeId('');
+        setDiscount(0);
+        setAfterDiscountTotal(0);
+        setIsPromo(false);
+        // setError('');
+    };
+
+    return {
+        promoCode,
+        setPromoCode,
+        error,
+        discount,
+        afterDiscountTotal,
+        promoCodeId,
+        isPromo,
+        handleApplyPromoCode,
+        resetPromoCode,
+    };
+};
+
+export default useApplyPromocode;
